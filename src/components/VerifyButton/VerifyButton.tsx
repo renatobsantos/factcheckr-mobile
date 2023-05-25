421c3b51a8d954251e804ee6c58e5625ddddb558
renatobsantos <404renato@gmail.com>
2023-05-25 15:58:18 -0300

    [FNM-2] Verify Button

--- src/components/VerifyButton/VerifyButton.tsx
+++ src/components/VerifyButton/VerifyButton.tsx
@@ -1,29 +1,30 @@
-import { theme } from '@theme'
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 
+import { theme } from '@theme'
+
 const VerifyButton = () => {
-	return (
-		<TouchableOpacity style={styles.container} activeOpacity={0.8}>
-			<Text style={styles.text}>Verificar</Text>
-		</TouchableOpacity>
-	)
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
+      <Text style={styles.text}>Verificar</Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		width: '50%',
-		height: 50,
-		backgroundColor: theme.colors.purple,
-		borderRadius: 16,
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginTop: '5%',
-	},
-	text: {
-		fontSize: 20,
-		color: theme.colors.white,
-		fontFamily: theme.fonts.InterRegular,
-	},
+  container: {
+    width: '50%',
+    height: 50,
+    backgroundColor: theme.colors.purple,
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '5%',
+  },
+  text: {
+    fontSize: 20,
+    color: theme.colors.white,
+    fontFamily: theme.fonts.InterRegular,
+  },
 })
 
 export default VerifyButton
